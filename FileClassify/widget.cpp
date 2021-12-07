9105b699e4e0f6ac3a4d9f898815eb81c868058e
ZefinNg <wuzefeng517@foxmail.com>
2021-12-07 23:14:00 +0800

    添加文件分类demo

---
+++ FileClassify/widget.cpp
@@ -0,0 +1,49 @@
+﻿#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent),
+      m_fileFilterLineEdit(nullptr),
+      m_openDirBtn(nullptr),
+      m_dealFileBtn(nullptr),
+      m_gridLayout(nullptr)
+{
+    this->initView();
+
+    connect(m_openDirBtn,  SIGNAL(clicked()), this, SLOT(onOpenDirBtnClicked()));
+    connect(m_dealFileBtn, SIGNAL(clicked()), this, SLOT(onDealFileBtnClicked()));
+}
+
+Widget::~Widget()
+{
+
+}
+
+void Widget::onOpenDirBtnClicked()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Select Directory"), "C:",
+                                                    QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+    m_fileFilterLineEdit->setText(dir);
+}
+
+void Widget::onDealFileBtnClicked()
+{
+
+}
+
+void Widget::initView()
+{
+    this->resize(600, 480);
+
+    m_gridLayout         = new QHBoxLayout(this);
+
+    m_fileFilterLineEdit = new QLineEdit(this);
+    m_fileFilterLineEdit->setEnabled(false);
+
+    m_openDirBtn         = new QPushButton(tr("Open"), this);
+    m_dealFileBtn        = new QPushButton(tr("Deal"), this);
+
+    m_gridLayout->addWidget(m_fileFilterLineEdit);
+    m_gridLayout->addWidget(m_openDirBtn);
+    m_gridLayout->addWidget(m_dealFileBtn);
+}
---
+++ FileClassify/widget.h
@@ -0,0 +1,32 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QFileDialog>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+private slots:
+    void onOpenDirBtnClicked();
+    void onDealFileBtnClicked();
+
+private:
+    void initView();
+
+private:
+    QLineEdit   *m_fileFilterLineEdit;
+    QPushButton *m_openDirBtn;
+    QPushButton *m_dealFileBtn;
+    QHBoxLayout *m_gridLayout;
+};
+
+#endif // WIDGET_H
